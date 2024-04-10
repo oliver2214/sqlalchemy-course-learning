da8c99cc3a8a8c97f8bc95c86b1e6ae5303c73c7
oliver2214 <dima.tovstokor@yandex.ru>
2024-04-10 15:21:51 +0300

    Урок 5. Добавить таблицу резюме

--- src/models.py
+++ src/models.py
@@ -1,11 +1,34 @@
-from sqlalchemy import Table, String, Integer, MetaData, Column
+import datetime
+import enum
+from sqlalchemy import ForeignKey, Table, String, Integer, MetaData, Column, text
+from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
+
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
 
 
 class WorkerORM(Base):
     __tablename__ = "workers"
     id = Column(Integer, primary_key=True)
     username = Column(String)
+
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    compensation: Mapped[int | None]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id"))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow
+    )
 
 
 metadata_obj = MetaData()
